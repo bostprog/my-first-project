80a809e7d3a98a9a4398188890f09c61dc960bea
bostprog <bostnoname@gmail.com>
2024-07-14 07:48:16 +0000

    add_adress_book

---
+++ adress_book.py
@@ -0,0 +1,16 @@
+people = {
+    'user_1': {
+        'name': 'John',
+        'age': 25,
+        'address': ('NY', 'Wall Street', 5586),
+        'grades': {'math': 5, 'physics': 4}
+    },
+    'user_2': {
+        'name': 'Yamamoto',
+        'age': 22,
+        'address': ('Tokio', 'Shibuya', 58),
+        'grades': {'math': 5, 'physics': 4}
+    },    
+}
+
+print(people[user_1][address])
